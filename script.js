45cc04d61a0c1ff8569d116e6879f9a4f5510891
sumanDevX19 <contactmrsumansaha@gmail.com>
2025-08-13 16:32:47 +0530

    Adind Milisecond counter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let [hours, minutes, seconds] = [0, 0, 0];
+let [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0];
 let timer = null;
 let running = false;
 
@@ -6,14 +6,19 @@ function updateDisplay() {
   let h = hours < 10 ? "0" + hours : hours;
   let m = minutes < 10 ? "0" + minutes : minutes;
   let s = seconds < 10 ? "0" + seconds : seconds;
-  document.getElementById("display").innerText = `${h}:${m}:${s}`;
+  let ms = milliseconds < 10 ? "0" + milliseconds : milliseconds;
+  document.getElementById("display").innerText = `${h}:${m}:${s}.${ms}`;
 }
 
 function startTimer() {
   if (!running) {
     running = true;
     timer = setInterval(() => {
-      seconds++;
+      milliseconds++;
+      if (milliseconds === 100) {
+        milliseconds = 0;
+        seconds++;
+      }
       if (seconds === 60) {
         seconds = 0;
         minutes++;
@@ -23,7 +28,7 @@ function startTimer() {
         hours++;
       }
       updateDisplay();
-    }, 1000);
+    }, 10);
   }
 }
 
@@ -34,7 +39,7 @@ function pauseTimer() {
 
 function resetTimer() {
   clearInterval(timer);
-  [hours, minutes, seconds] = [0, 0, 0];
+  [hours, minutes, seconds, milliseconds] = [0, 0, 0, 0];
   running = false;
   updateDisplay();
 }
